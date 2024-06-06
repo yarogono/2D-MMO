42b7762390f8358779f02b1140e9d41a90876b17
JeonHyuck Lim(John) <naocya@naver.com>
2024-06-06 18:19:39 +0900

    프로젝트 세팅 다시

---
+++ Client/Assets/@Scripts/@Managers/Managers.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers s_instance;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
