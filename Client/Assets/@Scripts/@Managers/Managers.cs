2b7e6f8dbc388aa53f15044f6baacd78585faf1d
JeonHyuck Lim(John) <naocya@naver.com>
2024-06-09 14:31:22 +0900

    기본 매니저 세팅

--- Client/Assets/@Scripts/@Managers/Managers.cs
+++ Client/Assets/@Scripts/@Managers/Managers.cs
@@ -1,16 +1,57 @@
+using System.Resources;
 using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
     private static Managers s_instance;
+    private static Managers Instance { get { Init(); return s_instance; } }
+
+    #region Contents
+    private GameManager _game = new GameManager();
+    private DataManager _data = new DataManager();
+    private ResourceManager _resource = new ResourceManager();
+    private SceneManagerEx _scene = new SceneManagerEx();
+    private UIManager _ui = new UIManager();
+    private WebManager _web = new WebManager();
+    private SoundManager _sound = new SoundManager();
+    #endregion
+
+    #region Core
+    #endregion
+
+    public static GameManager Game { get { return Instance._game; } }
+    public static DataManager Data { get { return Instance._data; } }
+    public static ResourceManager Resource { get { return Instance._resource;} }
+    public static SceneManagerEx Scene { get { return Instance._scene; } }
+    public static UIManager UI { get { return Instance._ui; } }
+    public static WebManager Web { get { return Instance._web; } }
+    public static SoundManager Sound { get { return Instance._sound; } }
 
     void Start()
     {
-        
+        Init();
     }
-    
-    void Update()
+
+    private static void Init()
     {
-        
+        if (s_instance == null)
+        {
+            GameObject managers = GameObject.FindGameObjectWithTag("Managers");
+
+            if (managers == null)
+            {
+                managers = new GameObject() { name = "@Managers" };
+                managers.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(managers);
+            s_instance = managers.GetComponent<Managers>();
+        }
     }
+
+    public static void Clear()
+    {
+
+    }
+
 }
