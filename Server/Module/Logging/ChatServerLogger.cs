3face5a0b9f5b75f71924a579be71ed114d6a9c5
yarogono <naocya@naver.com>
2024-06-11 10:06:20 +0900

    zlogger 세팅 관련 코드 추가

--- Server/Module/Logging/ChatServerLogger.cs
+++ Server/Module/Logging/ChatServerLogger.cs
@@ -1,5 +1,61 @@
-﻿namespace Logging;
+﻿using ChatServer.Configuration;
+using Microsoft.Extensions.Logging;
+using ZLogger;
+using ZLogger.Providers;
 
-public class ChatServerLogger
+namespace Logging;
+
+public class ChatServerLogger : IChatServerLogger
 {
+    private readonly ChatServerConfiguration _chatServerConfiguration;
+
+    private ILogger<ChatServerLogger> _logger;
+
+    public void CreateLogger()
+    {
+        if (_logger != null)
+            return;
+
+        ILoggerFactory factory = LoggerFactory.Create(logging =>
+        {
+            logging.SetMinimumLevel(LogLevel.Trace);
+            logging.AddZLoggerFile(_chatServerConfiguration.LogFilePath);
+            logging.AddZLoggerRollingFile(options =>
+            {
+                // File name determined by parameters to be rotated
+                options.FilePathSelector = (timestamp, sequenceNumber) => $"logs/{timestamp.ToLocalTime():yyyy-MM-dd}_{sequenceNumber:000}.log";
+                // The period of time for which you want to rotate files at time intervals.
+                options.RollingInterval = RollingInterval.Day;
+                // Limit of size if you want to rotate by file size. (KB)
+                options.RollingSizeKB = 1024;
+            });
+            // Add ZLogger provider to ILoggingBuilder
+            logging.AddZLoggerConsole(options =>
+            {
+                options.UseJsonFormatter(formatter =>
+                {
+                    formatter.IncludeProperties = IncludeProperties.ParameterKeyValues;
+                });
+            });
+            // Output Structured Logging, setup options
+            // logging.AddZLoggerConsole(options => options.UseJsonFormatter());
+        });
+
+        _logger = factory.CreateLogger<ChatServerLogger>();
+    }
+
+    public void Error(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Info(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Warn(string message)
+    {
+        Console.WriteLine(message);
+    }
 }
--- Server/Module/Logging/IChatServerLogger.cs
+++ Server/Module/Logging/IChatServerLogger.cs
@@ -2,4 +2,7 @@
 
 public interface IChatServerLogger
 {
+    public void Info(string message);
+    public void Error(string message);
+    public void Warn(string message);
 }
