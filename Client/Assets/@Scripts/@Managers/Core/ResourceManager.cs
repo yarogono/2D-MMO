c1d1ad9fe694708e12cd8d98c482ad8185bae237
JeonHyuck Lim(John) <naocya@naver.com>
2024-06-16 12:52:59 +0900

    ResourceManager 코드 작성 완료

--- Client/Assets/@Scripts/@Managers/Core/ResourceManager.cs
+++ Client/Assets/@Scripts/@Managers/Core/ResourceManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -9,9 +10,9 @@ public class ResourceManager
     private Dictionary<string, UnityEngine.Object> _resources = new Dictionary<string, UnityEngine.Object>();
     private Dictionary<string, AsyncOperationHandle> _handles = new Dictionary<string, AsyncOperationHandle>();
 
-    public T Load<T>(string key) where T : Object
+    public T Load<T>(string key) where T : UnityEngine.Object
     {
-        if (_resources.TryGetValue(key, out Object resource))
+        if (_resources.TryGetValue(key, out UnityEngine.Object resource))
         {
             return resource as T;
         }
@@ -20,26 +21,26 @@ public class ResourceManager
     }
 
     #region Load Resource
-    public GameObject Instantiate(string path, Transform parent = null)
+    public GameObject Instantiate(string key, Transform parent = null, bool pooling = false)
     {
-        GameObject original = Load<GameObject>(path);
+        GameObject prefab = Load<GameObject>(key);
 
-        if (original != null)
+        if (prefab == null)
         {
-            Debug.Log($"Failed to load prefab : {path}");
+            Debug.Log($"Failed to load prefab : {key}");
             return null;
         }
 
-        GameObject gameObject = Object.Instantiate(original, parent);
-        gameObject.name = original.name;
+        if (pooling)
+        {
+            //return Managers.Pool.Pop(prefab);
+        }
+
+        GameObject gameObject = UnityEngine.Object.Instantiate(prefab, parent);
+        gameObject.name = gameObject.name;
 
         return gameObject;
     }
-    #endregion
-
-    #region Addresable
-
-    #endregion
 
     public void Destroy(GameObject go)
     {
@@ -48,6 +49,81 @@ public class ResourceManager
             return;
         }
 
+        //if (Managers.Pool.Push(go))
+        //{
+        //    return;
+        //}
+
         GameObject.Destroy(go);
     }
+    #endregion
+
+    #region Addresable
+    private void LoadAsync<T>(string key, Action<T> callback = null) where T : UnityEngine.Object
+    {
+        // Cache
+        if (_resources.TryGetValue(key, out UnityEngine.Object resource))
+        {
+            callback?.Invoke(resource as T);
+            return;
+        }
+
+        string loadKey = key;
+        if (key.Contains(".sprite"))
+        {
+            loadKey = $"{key}[{key.Replace(".sprite", "")}]";
+        }
+
+        var asyncOperation = Addressables.LoadAssetAsync<T>(loadKey);
+        asyncOperation.Completed += (op) =>
+        {
+            _resources.Add(key, op.Result);
+            _handles.Add(key, asyncOperation);
+            callback?.Invoke(op.Result);
+        };
+    }
+
+    public void LoadAsync<T>(string lable, Action<string, int, int> callback) where T : UnityEngine.Object
+    {
+        var opHandle = Addressables.LoadResourceLocationsAsync(lable, typeof(T));
+        opHandle.Completed += (op) =>
+        {
+            int loadCount = 0;
+            int totalCount = op.Result.Count;
+
+            foreach (var result in op.Result)
+            {
+                if (result.PrimaryKey.Contains(".sprite"))
+                {
+                    LoadAsync<Sprite>(result.PrimaryKey, (obj) =>
+                    {
+                        loadCount++;
+                        callback?.Invoke(result.PrimaryKey, loadCount, totalCount);
+                    });
+                }
+                else
+                {
+                    LoadAsync<T>(result.PrimaryKey, (obj) =>
+                    {
+                        loadCount++;
+                        callback.Invoke(result.PrimaryKey, loadCount, totalCount);
+                    });
+                }
+            }
+        };
+    }
+
+    public void Clear()
+    {
+        _resources.Clear();
+
+        foreach (var handle in _handles)
+        {
+            Addressables.Release(handle);
+        }
+
+        _handles.Clear();
+    }
+    #endregion
+
 }
