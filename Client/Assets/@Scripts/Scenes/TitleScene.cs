5328b47f3563e00e9bf1df32278efd36ac3d01aa
JeonHyuck Lim(John) <naocya@naver.com>
2024-06-18 05:27:46 +0900

    샘플 TitleScene 추가

--- Client/Assets/@Scripts/Scenes/TitleScene.cs
+++ Client/Assets/@Scripts/Scenes/TitleScene.cs
@@ -6,7 +6,15 @@ public class TitleScene : MonoBehaviour
 {
     void Start()
     {
-        
+        Managers.Resource.LoadAsync<Object>("PreLoad", (key, count, totalCount) =>
+        {
+            Debug.Log($"{key} {count}/{totalCount}");
+
+            if (count == totalCount)
+            {
+                //Managers.Data.Init();
+            }
+        });
     }
 
     void Update()
