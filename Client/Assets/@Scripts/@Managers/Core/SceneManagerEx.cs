5242ef721c49e3c13cc547187a0fa32f926811b9
yarogono <naocya@naver.com>
2024-06-19 13:46:03 +0900

    SceneManagerEx 코드 작성

--- Client/Assets/@Scripts/@Managers/Core/SceneManagerEx.cs
+++ Client/Assets/@Scripts/@Managers/Core/SceneManagerEx.cs
@@ -1,8 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class SceneManagerEx
 {
+    public BaseScene CurrentScene { get { return GameObject.FindObjectOfType<BaseScene>(); } }
+
+    public void LoadScene(Define.EScene type)
+    {
+        //Managers.Clear();
+        SceneManager.LoadScene(GetSceneName(type));
+    }
+
+    private string GetSceneName(Define.EScene type)
+    {
+        string name = System.Enum.GetName(typeof(Define.EScene), type);
+        return name;
+    }
 
     public void Clear()
     {
-
+        //CurrentScene.Clear();
     }
+
+
 }
