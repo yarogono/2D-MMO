9cad613c9247c6fcb5c75d41d837f1dbc51d379d
yarogono <naocya@naver.com>
2024-07-01 10:06:32 +0900

    Util 클래스 코드 분석 주석 추가

--- Client/Assets/@Scripts/Utils/Util.cs
+++ Client/Assets/@Scripts/Utils/Util.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public static class Util
 {
+    // GameObject에서 제네릭 타입 T의 컴포넌트를 가져온다.
+    // 만약 T 타입의 컴포넌트가 없다면, 해당 컴포넌트를 GameObject에 추가한다.
+    // 기존에 존재하거나 새로 추가된 T 타입의 컴포넌트를 반환한다.
     public static T GetOrAddComponent<T>(GameObject go) where T : UnityEngine.Component
     {
         T component = go.GetComponent<T>();
@@ -13,6 +16,7 @@ public static class Util
         return component;
     }
 
+    // GameObject의 name에 해당하는 GameObject 검색
     public static GameObject FindChild(GameObject go, string name = null, bool recursive = false)
     {
         Transform transform = FindChild<Transform>(go, name, recursive);
@@ -24,6 +28,8 @@ public static class Util
         return transform.gameObject;
     }
 
+    // Transform을 사용하여 GameObject의 자식 객체를 검색하고 특정 컴포넌트를 찾는 기능
+    // Transform을 사용 => GameObject의 계층 구조에 관련된 모든 작업을 Transform 컴포넌트를 통해 이루어짐
     public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UnityEngine.Object
     {
         if (go == null)
