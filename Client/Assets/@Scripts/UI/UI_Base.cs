25987afc2f6dd9ee8e692ff0db677309a7ac1b4a
JeonHyuck Lim(John) <naocya@naver.com>
2024-07-07 20:54:27 +0900

    UI_Base 코드 분석 & 주석 추가

--- Client/Assets/@Scripts/UI/UI_Base.cs
+++ Client/Assets/@Scripts/UI/UI_Base.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+// UI 클래스에서 사용할 Base 함수들을 모아놓은 클래스
 public class UI_Base : InitBase
 {
     protected Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
 
-
+    // 유니티 엔진의 실행 순서 중 Awake에서 Init 초기화 함수를 호출하도록 합니다.
     public void Awake()
     {
         Init();
     }
 
+    // UI 객체와 맵핑해서 Bind 하는 함수입니다.
     public void Bind<T>(Type type) where T : UnityEngine.Object
     {
         string[] names = Enum.GetNames(type);
@@ -39,12 +41,14 @@ public class UI_Base : InitBase
         }
     }
 
+    // UI에 있는 다양한 타입들을 Bind 하도록 하는 함수입니다.
     protected void BindObjects(Type type) { Bind<GameObject>(type); }
     protected void BindImages(Type type) { Bind<Image>(type); }
     protected void BindTexts(Type type) { Bind<TMP_Text>(type); }
     protected void BindButtons(Type type) { Bind<Button>(type); }
     protected void BindToggles(Type type) { Bind<Toggle>(type); }
 
+    // Enum 값을 int로 변경해서 Bind된 UI 클래스를 가져오는(Get)하는 함수입니다.
     protected T Get<T>(int idx) where T : UnityEngine.Object
     {
         UnityEngine.Object[] objects = null;
@@ -56,12 +60,14 @@ public class UI_Base : InitBase
         return objects[idx] as T;
     }
 
+    // 각 UI에 맞는 타입들의 가져오는(Get) 하는 함수입니다.
     protected GameObject GetObject(int idx) { return Get<GameObject>(idx); }
     protected TMP_Text GetText(int idx) { return Get<TMP_Text>(idx); }
     protected Button GetButton(int idx) { return Get<Button>(idx); }
     protected Image GetImage(int idx) { return Get<Image>(idx); }
     protected Toggle GetToggle(int idx) { return Get<Toggle>(idx); }
 
+    // UI 타입들에 Event를 Bind하는 함수입니다.
     public static void BindEvent(GameObject go, Action<PointerEventData> action = null, Define.EUIEvent type = Define.EUIEvent.Click)
     {
         UI_EventHandler evt = Util.GetOrAddComponent<UI_EventHandler>(go);
