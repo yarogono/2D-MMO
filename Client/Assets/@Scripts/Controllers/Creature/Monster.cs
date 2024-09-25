c869793f3b3e311898db4bf481c27a1d6063b23d
yarogono <naocya@naver.com>
2024-09-25 13:26:07 +0900

    몬스터 AI

--- Client/Assets/@Scripts/Controllers/Creature/Monster.cs
+++ Client/Assets/@Scripts/Controllers/Creature/Monster.cs
@@ -3,6 +3,33 @@ using static Define;
 
 public class Monster : Creature
 {
+    public override ECreatureState CreatureState 
+    {
+        get { return base.CreatureState; }
+        set
+        {
+            if (_creatureState != value)
+            {
+                base.CreatureState = value;
+                switch (value)
+                {
+                    case ECreatureState.Idle:
+                        UpdateAITick = 0.5f;
+                        break;
+                    case ECreatureState.Move:
+                        UpdateAITick = 0.0f;
+                        break;
+                    case ECreatureState.Skill:
+                        UpdateAITick = 0.0f;
+                        break;
+                    case ECreatureState.Dead:
+                        UpdateAITick = 1.0f;
+                        break;
+                }
+            }
+        }
+    }
+
     public override bool Init()
     { 
         if (base.Init() == false)
@@ -19,9 +46,56 @@ public class Monster : Creature
 
 
     #region AI
+    public float SearchDistance { get; private set; } = 8.0f;
+    public float AttackDistance { get; private set; } = 4.0f;
+    Creature _target;
+    Vector3 _destPos;
+    Vector3 _initPos;
+
     protected override void UpdateIdle()
     {
         Debug.Log("Idle");
+
+        // Patrol
+        {
+            int patrolPercent = 10;
+            int rand = Random.Range(0, 100);
+            if (rand <= patrolPercent)
+            {
+                _destPos = _initPos + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2));
+                CreatureState = ECreatureState.Move;
+                return;
+            }
+        }
+
+        // Search Player
+        {
+            Creature target = null;
+            float bestDistanceSqr = float.MaxValue;
+            float searchDistanceSqr = SearchDistance * SearchDistance;
+
+            foreach (Hero hero in Managers.Object.Heroes)
+            {
+                Vector3 dir = hero.transform.position - transform.position;
+                float distToTargetSqr = dir.sqrMagnitude;
+
+                Debug.Log(distToTargetSqr);
+
+                if (distToTargetSqr > searchDistanceSqr)
+                    continue;
+
+                if (distToTargetSqr > bestDistanceSqr)
+                    continue;
+
+                target = hero;
+                bestDistanceSqr = distToTargetSqr;
+            }
+
+            _target = target;
+
+            if (_target != null)
+                CreatureState = ECreatureState.Move;
+        }
     }
 
 
