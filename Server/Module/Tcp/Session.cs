0fde3863172fec3a4018616fc6f1a596443a7bbb
JeonHyuck Lim(John) <naocya@naver.com>
2024-10-13 07:16:52 +0900

    TCP 모듈 기본 폴더 & 클래스 파일 생성

---
+++ Server/Module/Tcp/Session.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcp
+{
+    public abstract class Session
+    {
+        Socket _socket;
+        int _disconnected = 0;
+
+
+        public SocketAsyncEventArgs ReceiveEventArgs { get; private set; }
+        public SocketAsyncEventArgs SendEventArgs { get; private set; }
+
+        void Clear()
+        {
+
+        }
+
+
+    }
+}
