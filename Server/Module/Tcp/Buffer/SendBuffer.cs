2e7943edc01843c1043088448aca794570716b44
JeonHyuck Lim(John) <naocya@naver.com>
2024-10-15 00:06:19 +0900

    서버 SendBuffer 코드 추가

--- Server/Module/Tcp/Buffer/SendBuffer.cs
+++ Server/Module/Tcp/Buffer/SendBuffer.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tcp.Buffer
+﻿namespace Tcp.Buffer
 {
+    public class SendBufferHelper
+    {
+        public static ThreadLocal<SendBuffer> CurrentBuffer = new ThreadLocal<SendBuffer>(() => { return null; });
+        public static int ChunkSize { get; set; } = 4096 * 100;
+        public static ArraySegment<byte> Open(int reserveSize)
+        {
+            if (CurrentBuffer.Value == null)
+                CurrentBuffer.Value = new SendBuffer(ChunkSize);
+
+            if (CurrentBuffer.Value.FreeSize < reserveSize)
+                CurrentBuffer.Value = new SendBuffer(ChunkSize);
+
+            return CurrentBuffer.Value.Open(reserveSize);
+        }
+
+        public static ArraySegment<byte> Close(int usedSize)
+        {
+            return CurrentBuffer.Value.Close(usedSize);
+        }
+    }
+
     public class SendBuffer
     {
         byte[] _buffer;
