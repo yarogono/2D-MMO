2f0c15ee761d682ff98dd70c3c91ff9eb9fd64a0
JeonHyuck Lim(John) <naocya@naver.com>
2024-10-23 23:04:41 +0900

    Connector 코드 추가

---
+++ Server/Module/Tcp/Connector.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ZLogger;
+
+namespace Tcp
+{
+    public class Connector
+    {
+        Func<Session> _sessionFactory;
+
+        SocketAsyncEventArgsPool ReceiveEventArgsPool;
+        SocketAsyncEventArgsPool SendEventArgsPool;
+
+        private readonly ILogger<Connector> _logger;
+
+        public Connector(ILogger<Connector> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Connect(IPEndPoint endPoint, Func<Session> sessionFactory, int count = 1)
+        {
+            CreateEventArgsPool(count);
+            for (int i = 0; i < count; i++)
+            {
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _sessionFactory = sessionFactory;
+
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += OnConnectCompleted;
+                args.RemoteEndPoint = endPoint;
+                args.UserToken = socket;
+
+                RegisterConnect(args);
+            }
+        }
+
+        void RegisterConnect(SocketAsyncEventArgs args)
+        {
+            Socket socket = args.UserToken as Socket;
+            if (socket == null)
+                return;
+
+            try
+            {
+                bool pending = socket.ConnectAsync(args);
+                if (pending == false)
+                    OnConnectCompleted(null, args);
+            }
+            catch (Exception ex)
+            {
+                _logger.ZLogError($"Connector: {ex.Message}");
+            }
+        }
+
+        void OnConnectCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            try
+            {
+                if (args.SocketError == SocketError.Success)
+                {
+                    Session session = _sessionFactory.Invoke();
+                    session.SetEventArgs(ReceiveEventArgsPool.Pop(), SendEventArgsPool.Pop());
+                    session.Start(args.ConnectSocket, ReceiveEventArgsPool, SendEventArgsPool);
+                    session.OnConnected(args.RemoteEndPoint);
+                }
+                else
+                {
+                    _logger.ZLogError($"OnConnectCompleted Fail: {args.SocketError}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.ZLogError($"Connector: {ex.Message}");
+            }
+        }
+
+        void CreateEventArgsPool(int maxConnectionCount)
+        {
+            ReceiveEventArgsPool = new SocketAsyncEventArgsPool();
+
+            SendEventArgsPool = new SocketAsyncEventArgsPool();
+
+            SocketAsyncEventArgs arg;
+
+            for (int i = 0; i < maxConnectionCount; i++)
+            {
+                // ReceiveEventArgsPool
+                {
+                    arg = new SocketAsyncEventArgs();
+                    ReceiveEventArgsPool.Push(arg);
+                }
+
+                // SendEventArgsPool
+                {
+                    arg = new SocketAsyncEventArgs();
+                    SendEventArgsPool.Push(arg);
+                }
+            }
+        }
+    }
+}
