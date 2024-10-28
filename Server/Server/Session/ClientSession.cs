2cc1f4ae485e375fc90757d881129ac2cce28086
JeonHyuck Lim(John) <naocya@naver.com>
2024-10-28 23:41:33 +0900

    Server 프로젝트 코드 작성 중

--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
+using Tcp;
 
 namespace Server.Session
 {
-    internal class ClientSession
+    public class ClientSession : PacketSession
     {
+        public ClientSession(ILogger<Tcp.Session> logger) : base(logger)
+        {
+        }
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
     }
 }
--- Server/Server/Session/SesionManager.cs
+++ Server/Server/Session/SesionManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Server.Session
 {
-    internal class ServerSession
+    class SesionManager
     {
     }
 }
