22ceb4a199994d94ad8ea799da499b7d08e20cd3
yarogono <naocya@naver.com>
2024-11-06 09:37:26 +0900

    GameLogic & GameRoom 코드 추가

---
+++ Server/Server/Game/Room/GameRoom.cs
@@ -0,0 +1,45 @@
+﻿using Server.Game.Job;
+
+namespace Server.Game.Room
+{
+    public class GameRoom : JobSerializer
+    {
+        public int RoomId { get; set; }
+
+        //Dictionary<int, Player> _players = new Dictionary<int, Player>();
+
+        public void Init()
+        {
+
+        }
+
+
+        // 누군가 주기적으로 호출해줘야 한다
+        public void Update()
+        {
+            Flush();
+        }
+
+        //public void EnterGame(GameObject gameObject)
+        //{
+        //}
+
+
+        //public void Broadcast(IMessage packet)
+        //{
+        //    foreach (Player p in _players.Values)
+        //    {
+        //        p.Session.Send(packet);
+        //    }
+        //}
+
+        //public void Broadcast(IMessage packet, int exceptPlayerId)
+        //{
+        //    foreach (Player p in _players.Values)
+        //    {
+        //        if (p.Id != exceptPlayerId)
+        //            p.Session.Send(packet);
+        //    }
+        //}
+    }
+}
