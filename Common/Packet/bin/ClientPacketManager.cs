829e19a68b834252a46492f05010c6940334450c
yarogono <naocya@naver.com>
2024-11-11 09:39:08 +0900

    Common 추가

---
+++ Common/Packet/bin/ClientPacketManager.cs
@@ -0,0 +1,93 @@
+using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+
+class PacketManager
+{
+	#region Singleton
+	static PacketManager _instance = new PacketManager();
+	public static PacketManager Instance { get { return _instance; } }
+	#endregion
+
+	PacketManager()
+	{
+		Register();
+	}
+
+	Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>> _onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>>();
+	Dictionary<ushort, Action<PacketSession, IMessage>> _handler = new Dictionary<ushort, Action<PacketSession, IMessage>>();
+	
+	public Action<PacketSession, IMessage, ushort> CustomHandler { get; set; }
+
+	public void Register()
+	{		
+		_onRecv.Add((ushort)MsgId.SEnterGame, MakePacket<S_EnterGame>);
+		_handler.Add((ushort)MsgId.SEnterGame, PacketHandler.S_EnterGameHandler);		
+		_onRecv.Add((ushort)MsgId.SLeaveGame, MakePacket<S_LeaveGame>);
+		_handler.Add((ushort)MsgId.SLeaveGame, PacketHandler.S_LeaveGameHandler);		
+		_onRecv.Add((ushort)MsgId.SSpawn, MakePacket<S_Spawn>);
+		_handler.Add((ushort)MsgId.SSpawn, PacketHandler.S_SpawnHandler);		
+		_onRecv.Add((ushort)MsgId.SDespawn, MakePacket<S_Despawn>);
+		_handler.Add((ushort)MsgId.SDespawn, PacketHandler.S_DespawnHandler);		
+		_onRecv.Add((ushort)MsgId.SMove, MakePacket<S_Move>);
+		_handler.Add((ushort)MsgId.SMove, PacketHandler.S_MoveHandler);		
+		_onRecv.Add((ushort)MsgId.SSpawnItem, MakePacket<S_SpawnItem>);
+		_handler.Add((ushort)MsgId.SSpawnItem, PacketHandler.S_SpawnItemHandler);		
+		_onRecv.Add((ushort)MsgId.SPing, MakePacket<S_Ping>);
+		_handler.Add((ushort)MsgId.SPing, PacketHandler.S_PingHandler);		
+		_onRecv.Add((ushort)MsgId.SSwapWeaponItem, MakePacket<S_SwapWeaponItem>);
+		_handler.Add((ushort)MsgId.SSwapWeaponItem, PacketHandler.S_SwapWeaponItemHandler);		
+		_onRecv.Add((ushort)MsgId.SLootItem, MakePacket<S_LootItem>);
+		_handler.Add((ushort)MsgId.SLootItem, PacketHandler.S_LootItemHandler);		
+		_onRecv.Add((ushort)MsgId.SMeleeAttack, MakePacket<S_MeleeAttack>);
+		_handler.Add((ushort)MsgId.SMeleeAttack, PacketHandler.S_MeleeAttackHandler);		
+		_onRecv.Add((ushort)MsgId.SDamageMelee, MakePacket<S_DamageMelee>);
+		_handler.Add((ushort)MsgId.SDamageMelee, PacketHandler.S_DamageMeleeHandler);		
+		_onRecv.Add((ushort)MsgId.SDamageBullet, MakePacket<S_DamageBullet>);
+		_handler.Add((ushort)MsgId.SDamageBullet, PacketHandler.S_DamageBulletHandler);		
+		_onRecv.Add((ushort)MsgId.SGunAttack, MakePacket<S_GunAttack>);
+		_handler.Add((ushort)MsgId.SGunAttack, PacketHandler.S_GunAttackHandler);
+	}
+
+	public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer)
+	{
+		ushort count = 0;
+
+		ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+		count += 2;
+		ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+		count += 2;
+
+		Action<PacketSession, ArraySegment<byte>, ushort> action = null;
+		if (_onRecv.TryGetValue(id, out action))
+			action.Invoke(session, buffer, id);
+	}
+
+	void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer, ushort id) where T : IMessage, new()
+	{
+		T pkt = new T();
+		pkt.MergeFrom(buffer.Array, buffer.Offset + 4, buffer.Count - 4);
+		
+		if (CustomHandler != null)
+		{
+			CustomHandler.Invoke(session, pkt, id);
+		}
+		else
+		{
+			Action<PacketSession, IMessage> action = null;
+			if (_handler.TryGetValue(id, out action))
+				action.Invoke(session, pkt);
+		}
+
+	}
+
+	public Action<PacketSession, IMessage> GetPacketHandler(ushort id)
+	{
+		Action<PacketSession, IMessage> action = null;
+		if (_handler.TryGetValue(id, out action))
+			return action;
+		return null;
+	}
+}
---
+++ Common/Packet/bin/ServerPacketManager.cs
@@ -0,0 +1,87 @@
+using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+
+class PacketManager
+{
+	#region Singleton
+	static PacketManager _instance = new PacketManager();
+	public static PacketManager Instance { get { return _instance; } }
+	#endregion
+
+	PacketManager()
+	{
+		Register();
+	}
+
+	Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>> _onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>>();
+	Dictionary<ushort, Action<PacketSession, IMessage>> _handler = new Dictionary<ushort, Action<PacketSession, IMessage>>();
+	
+	public Action<PacketSession, IMessage, ushort> CustomHandler { get; set; }
+
+	public void Register()
+	{		
+		_onRecv.Add((ushort)MsgId.CEnterGame, MakePacket<C_EnterGame>);
+		_handler.Add((ushort)MsgId.CEnterGame, PacketHandler.C_EnterGameHandler);		
+		_onRecv.Add((ushort)MsgId.CLeaveGame, MakePacket<C_LeaveGame>);
+		_handler.Add((ushort)MsgId.CLeaveGame, PacketHandler.C_LeaveGameHandler);		
+		_onRecv.Add((ushort)MsgId.CMove, MakePacket<C_Move>);
+		_handler.Add((ushort)MsgId.CMove, PacketHandler.C_MoveHandler);		
+		_onRecv.Add((ushort)MsgId.CPong, MakePacket<C_Pong>);
+		_handler.Add((ushort)MsgId.CPong, PacketHandler.C_PongHandler);		
+		_onRecv.Add((ushort)MsgId.CSwapWeaponItem, MakePacket<C_SwapWeaponItem>);
+		_handler.Add((ushort)MsgId.CSwapWeaponItem, PacketHandler.C_SwapWeaponItemHandler);		
+		_onRecv.Add((ushort)MsgId.CLootItem, MakePacket<C_LootItem>);
+		_handler.Add((ushort)MsgId.CLootItem, PacketHandler.C_LootItemHandler);		
+		_onRecv.Add((ushort)MsgId.CMeleeAttack, MakePacket<C_MeleeAttack>);
+		_handler.Add((ushort)MsgId.CMeleeAttack, PacketHandler.C_MeleeAttackHandler);		
+		_onRecv.Add((ushort)MsgId.CDamageMelee, MakePacket<C_DamageMelee>);
+		_handler.Add((ushort)MsgId.CDamageMelee, PacketHandler.C_DamageMeleeHandler);		
+		_onRecv.Add((ushort)MsgId.CDamageBullet, MakePacket<C_DamageBullet>);
+		_handler.Add((ushort)MsgId.CDamageBullet, PacketHandler.C_DamageBulletHandler);		
+		_onRecv.Add((ushort)MsgId.CGunAttack, MakePacket<C_GunAttack>);
+		_handler.Add((ushort)MsgId.CGunAttack, PacketHandler.C_GunAttackHandler);
+	}
+
+	public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer)
+	{
+		ushort count = 0;
+
+		ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+		count += 2;
+		ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+		count += 2;
+
+		Action<PacketSession, ArraySegment<byte>, ushort> action = null;
+		if (_onRecv.TryGetValue(id, out action))
+			action.Invoke(session, buffer, id);
+	}
+
+	void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer, ushort id) where T : IMessage, new()
+	{
+		T pkt = new T();
+		pkt.MergeFrom(buffer.Array, buffer.Offset + 4, buffer.Count - 4);
+		
+		if (CustomHandler != null)
+		{
+			CustomHandler.Invoke(session, pkt, id);
+		}
+		else
+		{
+			Action<PacketSession, IMessage> action = null;
+			if (_handler.TryGetValue(id, out action))
+				action.Invoke(session, pkt);
+		}
+
+	}
+
+	public Action<PacketSession, IMessage> GetPacketHandler(ushort id)
+	{
+		Action<PacketSession, IMessage> action = null;
+		if (_handler.TryGetValue(id, out action))
+			return action;
+		return null;
+	}
+}
